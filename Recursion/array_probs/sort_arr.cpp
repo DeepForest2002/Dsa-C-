c32665dd09931b01c6fe7df1a9c5278a588a826e
Sayan Pramanik <pramaniksayan123@gmail.com>
2025-12-09 23:43:01 +0530

    recursion on array

---
+++ Recursion/array_probs/sort_arr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool check_arr(vector<int> arr, int n, int i)
+{
+    if (n == 0 || i == n - 1)
+        return true;
+    if (arr[i] > arr[i + 1])
+        return false;
+    return check_arr(arr, n, i += 1);
+}
+
+int find_element(vector<int> arr, int target, int n, int i)
+{
+    if (i > n - 1)
+        return -1;
+    if (arr[i] == target)
+        return i;
+    return find_element(arr, target, n, i += 1);
+}
+
+int main()
+{
+    vector<int> arr = {1, 4, 3, 0};
+    int n = arr.size();
+    int i = 0;
+    cout << check_arr(arr, n, i) << endl;
+    cout << find_element(arr, 9, n, 0);
+    return 0;
+}
