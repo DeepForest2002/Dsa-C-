0bc2691c54347cf5f20114fdba9b2c2949811c9e
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-01-29 23:10:54 +0530

    Max consecutive ones optimal

--- Sliding_window/max_ones.cpp
+++ Sliding_window/max_ones.cpp
@@ -22,9 +22,30 @@ int find_length(vector<int> &arr, int k)
     return maxlen;
 }
 
+int find_length2(vector<int> &arr, int k)
+{
+    int r = 0, l = 0, zerCount = 0;
+    int maxlen = 0;
+    while (r < arr.size())
+    {
+        if (arr[r] == 0)
+            zerCount += 1;
+        if (zerCount > k)
+        {
+            if (arr[l] == 0)
+                zerCount -= 1;
+            l += 1;
+        }
+        if (zerCount <= k)
+            maxlen = max(maxlen, (r - l + 1));
+        r += 1;
+    }
+    return maxlen;
+}
+
 int main()
 {
     vector<int> arr = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
-    cout << find_length(arr, 2);
+    cout << find_length2(arr, 2);
     return 0;
 }
