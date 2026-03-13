5d48b9fe042ed602c7aa2b9bf4e68841fd47a53a
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-03-13 22:33:24 +0530

    Binary Search added

---
+++ Searching/Part1/first-last.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int first(vector<int> arr, int target)
+{
+    int f = -1;
+    int low = 0, high = arr.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] >= target)
+        {
+            f = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return f;
+}
+int main()
+{
+    vector<int> arr = {3, 4, 13, 13, 13, 20, 40};
+    pair<int, int> ans;
+    ans.first = first(arr, 13);
+    cout << ans.first;
+    return 0;
+}
