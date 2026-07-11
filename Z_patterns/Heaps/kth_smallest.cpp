9e4cb16e41bb201d519d35f7dc58ded5c0d31335
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-07-11 23:47:45 +0530

    kth smallest

---
+++ Z_patterns/Heaps/kth_smallest.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(vector<int> &arr, int k)
+{
+    priority_queue<int> pq;
+    int i = 0;
+    for (int i = 0; i < k; i++)
+    {
+        pq.push(arr[i]);
+    }
+    for (int i = k; i < arr.size(); i++)
+    {
+        if (arr[i] >= pq.top())
+            continue;
+        else
+        {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+    return pq.top();
+}
+
+int main()
+{
+    return 0;
+}
