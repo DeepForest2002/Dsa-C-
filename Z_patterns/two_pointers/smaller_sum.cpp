1fd745d815941647d07d432184ccd61282b13d9a
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-07-13 22:47:19 +0530

    Smaller sum Problem

---
+++ Z_patterns/two_pointers/smaller_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int smaller_sum_count(vector<int>arr, int target){
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size() - 2;i++){
+        int l = i + 1, r = arr.size() - 1;
+        while(l<r){
+            
+        }
+    }
+}
+
+int main(){
+    vector<int> arr = {-2,0,1,3};
+    return 0;
+}
